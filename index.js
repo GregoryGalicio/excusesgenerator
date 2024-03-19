9fe7774913908d57f0760e2c784383de07f1eff5
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2024-03-19 18:13:11 -0500

    implementando primera funcion de frase aleatoria

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ let quien = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
 let accion = ['comió', 'pisó', 'aplastó', 'rompió'];
 let que = ['mi tarea', 'mi teléfono', 'el auto'];
 let cuando = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras oraba'];
+
+
+// console.log(quien.length)
+
+function azar(x,y,z,w) {
+    let letraAzar=[];
+    let numeroAleatoriox=Math.floor(Math.random()*x.length)
+    let numeroAleatorioy=Math.floor(Math.random()*y.length)
+    let numeroAleatorioz=Math.floor(Math.random()*z.length)
+    let numeroAleatoriow=Math.floor(Math.random()*w.length)
+    
+    let fraseAleatoria=`${x[numeroAleatoriox]} ${y[numeroAleatorioy]} ${z[numeroAleatorioz]} ${w[numeroAleatoriow]}`
+
+    
+
+    console.log(fraseAleatoria)
+}
+
+azar(quien,accion,que,cuando)
