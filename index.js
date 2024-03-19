d0d3d3cf52cea341609cf652e1d2d8ab6c470b49
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2024-03-19 17:43:37 -0500

    creando las listas

---
+++ index.js
@@ -0,0 +1,6 @@
+
+/*Mi perro se comió mi tarea*/
+let quien = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+let accion = ['comió', 'pisó', 'aplastó', 'rompió'];
+let que = ['mi tarea', 'mi teléfono', 'el auto'];
+let cuando = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras oraba'];
